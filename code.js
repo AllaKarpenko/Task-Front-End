c188186c7cb55a60fb42a08de00b08ad50af16d3
Alla Karpenko <alla.karpenko9999@gmail.com>
2022-09-14 10:25:41 +0300

    create index.html, code.js

---
+++ code.js
@@ -0,0 +1,4 @@
+const  priceBitcoin =prompt("What is Bitcoin price today?");
+const  dollars =prompt("How much $ do you have?");
+const BTC = dollars/priceBitcoin;
+alert (`You can buy ${BTC} BTC`);
